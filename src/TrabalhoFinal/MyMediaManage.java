d87b077bd5dff25998267422f24552a4689164f5
Jorge Junqueiro <jorelojj@gmail.com>
2025-12-13 16:49:07 +0000

    tabela formatada sem tabs

--- src/TrabalhoFinal/MyMediaManage.java
+++ src/TrabalhoFinal/MyMediaManage.java
@@ -8,7 +8,7 @@ public class MyMediaManage {
     int[] ano = new int[tamMax];
     boolean[] visto = new boolean[tamMax];
     int[] rating = new int[tamMax];
-    int nItens=10;
+    int nItens=3;
     titulo[0] = "The Matrix"; tipo[0] = 'F'; ano[0] = 1999; visto[0] = true; rating[0] = 9;
     titulo[1] = "Breaking Bad"; tipo[1] = 'S'; ano[1] = 2008; visto[1] = true; rating[1] = 10;
     titulo[2] = "Oppenheimer"; tipo[2] = 'F'; ano[2] = 2023; visto[2] = false; rating[2] = 0;
@@ -18,15 +18,27 @@ public class MyMediaManage {
 
     static void fazedorTabela(int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
         
-        System.out.println("Nº\tTítulo\t\tTipo\tAno\tVisto\tRating\n--------kk--teste1------------------------------------------------");
+        System.out.printf("%-4s%s%15s%15s%15s%15s\n----------------------------------------------------------------------\n", "No", "Titulo", "Tipo", "Ano", "Visto", "Rating");
         for(int i=0;i<=nItens;i++){
 
-            System.out.printf("%d\t%10s\t%c\t%d\t%b\t%d \n", i+1, titulo[i], tipo[i],ano[i],visto[i],rating[i]);
+            System.out.printf("%-4d%-18s%-15c%-14d%-14c%2d\n", i+1, titulo[i], tipo[i],ano[i],FazedorDeCruz(visto, i),rating[i]);
 
         }
 
 
     }
+
+    static char FazedorDeCruz(boolean visto[],int i){
+        //este metodo retorna x ou vazio conforme foi visto
+        char cruz='X', vazio= ' ';
+        if(visto[i]==true){
+            return cruz;
+        }else{
+            return vazio;
+        }
+
+    }
+
 
 
 }

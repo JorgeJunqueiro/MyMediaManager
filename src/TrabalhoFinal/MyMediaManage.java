64f1ad1a927e61a179741217a62ece674f9aa8b7
Hector <0x.hectorsantos@gmail.com>
2025-12-13 23:17:23 +0000

    void main foi para baixo

--- src/TrabalhoFinal/MyMediaManage.java
+++ src/TrabalhoFinal/MyMediaManage.java
@@ -1,24 +1,8 @@
 package TrabalhoFinal;
-
 import java.util.Scanner;
 
 public class MyMediaManage {
-    public static void main(String[] args) {
 
-    menuPrincipal();
-    int tamMax = 100;
-    String[] titulo = new String[tamMax];
-    char[] tipo = new char[tamMax];
-    int[] ano = new int[tamMax];
-    boolean[] visto = new boolean[tamMax];
-    int[] rating = new int[tamMax];
-    int nItens=3;
-    titulo[0] = "The Matrix"; tipo[0] = 'F'; ano[0] = 1999; visto[0] = true; rating[0] = 9;
-    titulo[1] = "Breaking Bad"; tipo[1] = 'S'; ano[1] = 2008; visto[1] = true; rating[1] = 10;
-    titulo[2] = "Oppenheimer"; tipo[2] = 'F'; ano[2] = 2023; visto[2] = false; rating[2] = 0;
-    titulo[3] = "Dark"; tipo[3] = 'S'; ano[3] = 2017; visto[3] = false; rating[3] = 0;
-    fazedorTabela(tamMax, titulo, tipo, ano, visto, rating, nItens);
-    }
 
     static void fazedorTabela(int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
         
@@ -47,6 +31,7 @@ public class MyMediaManage {
     //Metodo para Ler String
     static String lerString (){
 
+        @SuppressWarnings("resource")
         Scanner teclado = new Scanner(System.in);
         String resultado = teclado.nextLine();
         return resultado;
@@ -77,11 +62,6 @@ public class MyMediaManage {
     }
 
     
-
-
-    
-
-
 
     public static void limparTela() {
     try {
@@ -127,61 +107,79 @@ public class MyMediaManage {
 
     }
 
-            static void menuMarcar(){
-                //Este é o menu de marcação.
-                char opcao = 'a';
-                limparTela();
-                do{
-                    System.out.println("+==================   MENU  =======================+");
-                    System.out.printf("|%-25s %25s%n"," * Maracar como (V)isto por posição","|");
-                    System.out.printf("|%-25s %25s%n"," * Marcar como visto por (t)ítulo","|");
-                    System.out.printf("|%-25s %25s%n"," * (D)esmarcar último marcado como visto","|");
-                    System.out.printf("|%-25s %25s%n"," * (A)tribuir / alterar rating por números","|");
-                    System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
-                    System.out.println("+==================================================+");
-                    
-                    System.out.print("\n\nEscolha sua opcao: ");
-                    opcao = lerchar();
-                
-                }while(opcao!='v');
-        }
+    static void menuMarcar(){
+        //Este é o menu de marcação.
+        char opcao = 'a';
+        limparTela();
+        do{
+            System.out.println("+==================   MENU  =======================+");
+            System.out.printf("|%-25s %25s%n"," * Maracar como (V)isto por posição","|");
+            System.out.printf("|%-25s %25s%n"," * Marcar como visto por (t)ítulo","|");
+            System.out.printf("|%-25s %25s%n"," * (D)esmarcar último marcado como visto","|");
+            System.out.printf("|%-25s %25s%n"," * (A)tribuir / alterar rating por números","|");
+            System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
+            System.out.println("+==================================================+");
+            
+            System.out.print("\n\nEscolha sua opcao: ");
+            opcao = lerchar();
+        
+        }while(opcao!='v');
+    }
 
-            static void adicionador(){
-                //Este é o menu permite a adicionar ou eleminar conteudos.
-                char opcao = 'a';
-                limparTela();
-                do{
-                    System.out.println("+==================   MENU  =======================+");
-                    System.out.printf("|%-25s %25s%n"," * (A)dicionar item no fim","|");
-                    System.out.printf("|%-25s %25s%n"," * Adicionar (i)tem na posição n","|");
-                    System.out.printf("|%-25s %25s%n"," * Apagar item na (p)osição n","|");
-                    System.out.printf("|%-25s %25s%n"," * Apagar itens (v)istos","|");
-                    System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
-                    System.out.println("+==================================================+");
-                    
-                    System.out.print("\n\nEscolha sua opcao: ");
-                    opcao = lerchar();
-                
-                }while(opcao!='v');
-        }
+    static void adicionador(){
+        //Este é o menu permite a adicionar ou eleminar conteudos.
+        char opcao = 'a';
+        limparTela();
+        do{
+            System.out.println("+==================   MENU  =======================+");
+            System.out.printf("|%-25s %25s%n"," * (A)dicionar item no fim","|");
+            System.out.printf("|%-25s %25s%n"," * Adicionar (i)tem na posição n","|");
+            System.out.printf("|%-25s %25s%n"," * Apagar item na (p)osição n","|");
+            System.out.printf("|%-25s %25s%n"," * Apagar itens (v)istos","|");
+            System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
+            System.out.println("+==================================================+");
+            
+            System.out.print("\n\nEscolha sua opcao: ");
+            opcao = lerchar();
+        
+        }while(opcao!='v');
+    }
 
-        static void Estatisticas(){
-                //Este é o menu permite o utilizador escolher que estatisticas pretende ver.
-                char opcao = 'a';
-                limparTela();
-                do{
-                    System.out.println("+==================   MENU  =======================+");
-                    System.out.printf("|%-25s %25s%n"," * (P)ercentagem de vistos","|");
-                    System.out.printf("|%-25s %25s%n"," * (M)édia de rating dos vistosn","|");
-                    System.out.printf("|%-25s %25s%n"," * Distribuição por (d)écada","|");
-                    System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
-                    System.out.println("+==================================================+");
-                    
-                    System.out.print("\n\nEscolha sua opcao: ");
-                    opcao = lerchar();
-                
-                }while(opcao!='v');
-        }
+    static void Estatisticas(){
+        //Este é o menu permite o utilizador escolher que estatisticas pretende ver.
+        char opcao = 'a';
+        limparTela();
+        do{
+            System.out.println("+==================   MENU  =======================+");
+            System.out.printf("|%-25s %25s%n"," * (P)ercentagem de vistos","|");
+            System.out.printf("|%-25s %25s%n"," * (M)édia de rating dos vistosn","|");
+            System.out.printf("|%-25s %25s%n"," * Distribuição por (d)écada","|");
+            System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
+            System.out.println("+==================================================+");
+            
+            System.out.print("\n\nEscolha sua opcao: ");
+            opcao = lerchar();
+        
+        }while(opcao!='v');
+    }
+
+    public static void main(String[] args) {
+
+    menuPrincipal();
+    int tamMax = 100;
+    String[] titulo = new String[tamMax];
+    char[] tipo = new char[tamMax];
+    int[] ano = new int[tamMax];
+    boolean[] visto = new boolean[tamMax];
+    int[] rating = new int[tamMax];
+    int nItens=3;
+    titulo[0] = "The Matrix"; tipo[0] = 'F'; ano[0] = 1999; visto[0] = true; rating[0] = 9;
+    titulo[1] = "Breaking Bad"; tipo[1] = 'S'; ano[1] = 2008; visto[1] = true; rating[1] = 10;
+    titulo[2] = "Oppenheimer"; tipo[2] = 'F'; ano[2] = 2023; visto[2] = false; rating[2] = 0;
+    titulo[3] = "Dark"; tipo[3] = 'S'; ano[3] = 2017; visto[3] = false; rating[3] = 0;
+    fazedorTabela(tamMax, titulo, tipo, ano, visto, rating, nItens);
+    
+    }
 
         
         

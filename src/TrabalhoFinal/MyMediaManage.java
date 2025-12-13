561456fa97609e3c3ed936311c0bd50ff4b4e131
Hector <0x.hectorsantos@gmail.com>
2025-12-13 17:02:07 +0000

    criacao menu,ler string,ler char, limpar tela 1305

--- src/TrabalhoFinal/MyMediaManage.java
+++ src/TrabalhoFinal/MyMediaManage.java
@@ -1,7 +1,11 @@
 package TrabalhoFinal;
 
+import java.util.Scanner;
+
 public class MyMediaManage {
     public static void main(String[] args) {
+
+    menuPrincipal();
     int tamMax = 100;
     String[] titulo = new String[tamMax];
     char[] tipo = new char[tamMax];
@@ -16,7 +20,7 @@ public class MyMediaManage {
     fazedorTabela(tamMax, titulo, tipo, ano, visto, rating, nItens);
     }
 
-    static void fazedorTabela(Int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
+    static void fazedorTabela(int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
         
         System.out.println("Nº\tTítulo\t\tTipo\tAno\tVisto\tRating\n--------kk--teste1------------------------------------------------");
         System.out.println("Teste1");
@@ -31,6 +35,65 @@ public class MyMediaManage {
 
     }
 
+    //Metodo para Ler String
+    static String lerString (){
+
+        Scanner teclado = new Scanner(System.in);
+        String resultado = teclado.nextLine();
+        
+        return resultado;
+
+    }
+
+    static char lerchar (){
+
+        
+        String palavra = lerString();
+        char opcao = palavra.charAt(0);
+
+        
+        return opcao;
+
+    }
+
+
+
+    public static void limparTela() {
+    try {
+        new ProcessBuilder("clear").inheritIO().start().waitFor();
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+    }
+
+    static void menuPrincipal (){
+
+        char opcao = 'a';
+
+       do{
+
+            limparTela();
+            System.out.println("+==================   MENU  =======================+");
+            System.out.printf("|%-25s %25s%n", "","|");
+            System.out.printf("|%-25s %25s%n"," * (V)isualizar","|");
+            System.out.printf("|%-25s %25s%n"," * (M)arcar / Classificar","|");
+            System.out.printf("|%-25s %25s%n"," * (E)ditar","|");
+            System.out.printf("|%-25s %25s%n"," * Es(t)atísticas","|");
+            System.out.printf("|%-25s %25s%n"," * (S)air","|");
+            System.out.printf("|%-25s %25s%n", "","|");
+            System.out.println("+==================================================+");
+
+
+
+            System.out.print("\n\nEscolha sua opcao: ");
+            opcao = lerchar();
+
+        }while (opcao != 's');
+         
+
+        
+
+    }
 
 }
 

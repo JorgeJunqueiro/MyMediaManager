e5d364b16211cd9a971e0b76045e867fd9a14f6b
Hector <0x.hectorsantos@gmail.com>
2025-12-13 23:42:00 +0000

    comentario com nomes

--- src/TrabalhoFinal/MyMediaManage.java
+++ src/TrabalhoFinal/MyMediaManage.java
@@ -1,75 +1,27 @@
+/*
+ * ============================================================
+ * Projeto: My Media Manager
+ *
+ * Alunos:
+ *   - Nome : Hector Silva
+ *     Nº   : 20250360
+ *     Curso: Engenharia Informática
+ *     Turma: 2
+ *
+ *   - Nome : Jorge Junqueiro
+ *     Nº   : XXXXXXXX
+ *     Curso: Engenharia Informática
+ *     Turma: 2
+ * ============================================================
+ */
+
+
+
+
 package TrabalhoFinal;
 import java.util.Scanner;
 
 public class MyMediaManage {
-
-
-    static void fazedorTabela(int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
-        
-        System.out.printf("%-4s%s%15s%15s%15s%15s\n----------------------------------------------------------------------\n", "No", "Titulo", "Tipo", "Ano", "Visto", "Rating");
-        for(int i=0;i<=nItens;i++){
-
-            System.out.printf("%-4d%-18s%-15c%-14d%-14c%2d\n", i+1, titulo[i], tipo[i],ano[i],FazedorDeCruz(visto, i),rating[i]);
-            
-        }
-    }
-
-
-
-    static char FazedorDeCruz(boolean visto[],int i){
-        //este metodo retorna x ou vazio conforme foi visto
-        char cruz='X', vazio= ' ';
-        if(visto[i]==true){
-            return cruz;
-        }else{
-            return vazio;
-        }
-
-    }
-
-
-    //Metodo para Ler String
-    static String lerString (){
-
-        @SuppressWarnings("resource")
-        Scanner teclado = new Scanner(System.in);
-        String resultado = teclado.nextLine();
-        return resultado;
-
-    }
-
-    // Metodo ler char e tranforma para minuscula
-    static char lerchar (){
-
-        System.out.print("\n\nEscolha sua opcao: ");
-        String palavra = lerString();
-
-        //Verifica se o usuario digitou mais que 1 letra
-        if(palavra.length() > 1){
-            System.out.println("\n**** Escolha apenas uma letra ****");
-            lerchar();
-        }
-
-        // Verificar de é Maisucla e converte para minuscula
-        char opcao = palavra.charAt(0);
-        if (opcao >= 'A' && opcao <= 'Z'){
-            int dif = 'a' - 'A';
-            return (char)(opcao+dif);
-        }
-        
-
-        return opcao;
-    }
-
-    
-
-    public static void limparTela() {
-    try {
-        new ProcessBuilder("clear").inheritIO().start().waitFor();
-    } catch (Exception e) {
-        System.out.println(e);
-    }
-    }
 
     static void menuPrincipal (){
 
@@ -90,7 +42,7 @@ public class MyMediaManage {
 
 
 
-            opcao = lerchar();
+            opcao = lerChar();
 
             switch (opcao) {
                 case ('v'):
@@ -107,6 +59,96 @@ public class MyMediaManage {
 
     }
 
+    //------------------------------------------------Metodo: Limpar a tela do terminal--------------------------------
+    public static void limparTela() {
+    try {
+        new ProcessBuilder("clear").inheritIO().start().waitFor();
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+    }
+
+
+    //------------------------------------------------Metodo: Ler String-----------------------------------------------
+    static String lerString (){
+
+        @SuppressWarnings("resource")
+        Scanner teclado = new Scanner(System.in);
+        String resultado = teclado.nextLine();
+        return resultado;
+
+    }
+
+    //------------------------------------------------Metodo: ler char e tranforma para minuscula se necessario--------
+    static char lerChar (){
+
+        System.out.print("\n\nEscolha sua opcao: ");
+        String palavra = lerString();
+
+        //Verifica se o usuario digitou mais que 1 letra
+        if(palavra.length() > 1){
+            System.out.println("\n**** Escolha apenas uma letra ****");
+            lerChar();
+        }
+
+        // Verificar de é Maisucla e converte para minuscula
+        char opcao = palavra.charAt(0);
+        if (opcao >= 'A' && opcao <= 'Z'){
+            int dif = 'a' - 'A';
+            return (char)(opcao+dif);
+        }
+        
+
+        return opcao;
+    }
+
+
+    //------------------------------------------------Metodo: Cria tabela com array dos filmes-------------------------
+    static void fazedorTabela(int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
+        
+        System.out.printf("%-4s%s%15s%15s%15s%15s\n----------------------------------------------------------------------\n", "No", "Titulo", "Tipo", "Ano", "Visto", "Rating");
+        for(int i=0;i<=nItens;i++){
+
+            System.out.printf("%-4d%-18s%-15c%-14d%-14c%2d\n", i+1, titulo[i], tipo[i],ano[i],FazedorDeCruz(visto, i),rating[i]);
+            
+        }
+    }
+
+
+    //------------------------------------------------Metodo: retorna x ou vazio conforme foi visto--------------------
+    static char FazedorDeCruz(boolean visto[],int i){
+      
+        char cruz='X', vazio= ' ';
+        if(visto[i]==true){
+            return cruz;
+        }else{
+            return vazio;
+        }
+
+    }
+
+     //------------------------------------------------Metodo: Menu Visualizar---------------------------------------------
+    static void menuVisualizar(){
+        //Este é o menu de marcação.
+        char opcao = 'a';
+        limparTela();
+        do{
+            System.out.println("+==================   MENU  =======================+");
+            System.out.printf("|%-25s %25s%n"," * Maracar como (V)isto por posição","|");
+            System.out.printf("|%-25s %25s%n"," * Marcar como visto por (t)ítulo","|");
+            System.out.printf("|%-25s %25s%n"," * (D)esmarcar último marcado como visto","|");
+            System.out.printf("|%-25s %25s%n"," * (A)tribuir / alterar rating por números","|");
+            System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
+            System.out.println("+==================================================+");
+            
+            System.out.print("\n\nEscolha sua opcao: ");
+            opcao = lerChar();
+        
+        }while(opcao!='v');
+    }
+
+
+    //------------------------------------------------Metodo: Menu Marcar---------------------------------------------
     static void menuMarcar(){
         //Este é o menu de marcação.
         char opcao = 'a';
@@ -121,12 +163,13 @@ public class MyMediaManage {
             System.out.println("+==================================================+");
             
             System.out.print("\n\nEscolha sua opcao: ");
-            opcao = lerchar();
+            opcao = lerChar();
         
         }while(opcao!='v');
     }
 
-    static void adicionador(){
+    //------------------------------------------------Metodo: Menu Editar--------------------------------------------
+    static void menuEditar(){
         //Este é o menu permite a adicionar ou eleminar conteudos.
         char opcao = 'a';
         limparTela();
@@ -140,12 +183,13 @@ public class MyMediaManage {
             System.out.println("+==================================================+");
             
             System.out.print("\n\nEscolha sua opcao: ");
-            opcao = lerchar();
+            opcao = lerChar();
         
         }while(opcao!='v');
     }
 
-    static void Estatisticas(){
+    //------------------------------------------------Metodo: Menu Estatistica----------------------------------------
+    static void menuEstatisticas(){
         //Este é o menu permite o utilizador escolher que estatisticas pretende ver.
         char opcao = 'a';
         limparTela();
@@ -158,10 +202,13 @@ public class MyMediaManage {
             System.out.println("+==================================================+");
             
             System.out.print("\n\nEscolha sua opcao: ");
-            opcao = lerchar();
+            opcao = lerChar();
         
         }while(opcao!='v');
     }
+
+
+    //==============================     MAIN     ===============================================
 
     public static void main(String[] args) {
 
@@ -178,10 +225,8 @@ public class MyMediaManage {
     titulo[2] = "Oppenheimer"; tipo[2] = 'F'; ano[2] = 2023; visto[2] = false; rating[2] = 0;
     titulo[3] = "Dark"; tipo[3] = 'S'; ano[3] = 2017; visto[3] = false; rating[3] = 0;
     fazedorTabela(tamMax, titulo, tipo, ano, visto, rating, nItens);
-    
     }
 
-        
         
 
 }

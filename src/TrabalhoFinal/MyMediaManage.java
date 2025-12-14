c0fc898c37ac61891fcd417792e6f07ffa754348
Hector <0x.hectorsantos@gmail.com>
2025-12-14 00:54:02 +0000

    codigo organizado, menu voltar implementado

--- src/TrabalhoFinal/MyMediaManage.java
+++ src/TrabalhoFinal/MyMediaManage.java
@@ -23,12 +23,13 @@ import java.util.Scanner;
 
 public class MyMediaManage {
 
-    static void menuPrincipal (){
+    static void menuPrincipal (int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
 
-        char opcao = 'a';
-        limparTela();
-
+       char opcao = 'a';
+        
+       limparTela();
        do{
+            
 
             System.out.println("+==================   MENU  =======================+");
             System.out.printf("|%-25s %25s%n", "","|");
@@ -46,11 +47,20 @@ public class MyMediaManage {
 
             switch (opcao) {
                 case ('v'):
-                    System.out.println("Menu Visualizar\n\n\n");
-                    
+                    menuVisualizar(tamMax, titulo, tipo, ano, visto, rating, nItens);
                     break;
+                case ('m'):
+                    menuMarcar(tamMax, titulo, tipo, ano, visto, rating, nItens);
+                    break;
+                case ('e'):
+                    menuEditar(tamMax, titulo, tipo, ano, visto, rating, nItens);
+                    break;
+                case ('t'):
+                    menuEstatisticas(tamMax, titulo, tipo, ano, visto, rating, nItens);
+                    break;    
             
                 default:
+                    System.out.println("**** Opcao invalida ****\n\n");
                     break;
             }
 
@@ -103,15 +113,21 @@ public class MyMediaManage {
     }
 
 
+
+
     //------------------------------------------------Metodo: Cria tabela com array dos filmes-------------------------
     static void fazedorTabela(int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
+
+        limparTela();
         
         System.out.printf("%-4s%s%15s%15s%15s%15s\n----------------------------------------------------------------------\n", "No", "Titulo", "Tipo", "Ano", "Visto", "Rating");
-        for(int i=0;i<=nItens;i++){
+        for(int i=0;i<nItens;i++){
 
             System.out.printf("%-4d%-18s%-15c%-14d%-14c%2d\n", i+1, titulo[i], tipo[i],ano[i],FazedorDeCruz(visto, i),rating[i]);
             
         }
+
+        System.out.println("\n\n\n");
     }
 
 
@@ -128,103 +144,157 @@ public class MyMediaManage {
     }
 
      //------------------------------------------------Metodo: Menu Visualizar---------------------------------------------
-    static void menuVisualizar(){
-        //Este é o menu de marcação.
+    static void menuVisualizar(int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
         char opcao = 'a';
         limparTela();
         do{
-            System.out.println("+==================   MENU  =======================+");
-            System.out.printf("|%-25s %25s%n"," * Maracar como (V)isto por posição","|");
-            System.out.printf("|%-25s %25s%n"," * Marcar como visto por (t)ítulo","|");
-            System.out.printf("|%-25s %25s%n"," * (D)esmarcar último marcado como visto","|");
-            System.out.printf("|%-25s %25s%n"," * (A)tribuir / alterar rating por números","|");
-            System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
+            System.out.println("+==================================================+");
+            System.out.printf("|%-50s|%n", "");
+            System.out.printf("|%-50s|%n", " * Visualizar (t)odos");
+            System.out.printf("|%-50s|%n", " * Visualizar por (a)no exato");
+            System.out.printf("|%-50s|%n", " * Visualizar por (n)ão vistos");
+            System.out.printf("|%-50s|%n", " * Visualizar por (p)alavra no título");
+            System.out.printf("|%-50s|%n", " * Visualizar por (r)ating mínimo");
+            System.out.printf("|%-50s|%n", " * Visualização (m)ulticritério");
+            System.out.printf("|%-50s|%n", " * (V)oltar");
+            System.out.printf("|%-50s|%n", "");
             System.out.println("+==================================================+");
             
-            System.out.print("\n\nEscolha sua opcao: ");
             opcao = lerChar();
+
+            
+
+            switch (opcao) {
+                case ('t'):
+                    fazedorTabela(tamMax, titulo, tipo, ano, visto, rating, nItens);
+                    break;
+
+                case ('v'):
+                    menuPrincipal(tamMax, titulo, tipo, ano, visto, rating, nItens);
+                    break;
+               
+            
+                default:
+                    System.out.println("**** Opcao invalida ****\n\n");
+                    break;
+            }
         
         }while(opcao!='v');
     }
 
 
     //------------------------------------------------Metodo: Menu Marcar---------------------------------------------
-    static void menuMarcar(){
-        //Este é o menu de marcação.
+    static void menuMarcar(int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
         char opcao = 'a';
         limparTela();
         do{
             System.out.println("+==================   MENU  =======================+");
-            System.out.printf("|%-25s %25s%n"," * Maracar como (V)isto por posição","|");
-            System.out.printf("|%-25s %25s%n"," * Marcar como visto por (t)ítulo","|");
-            System.out.printf("|%-25s %25s%n"," * (D)esmarcar último marcado como visto","|");
-            System.out.printf("|%-25s %25s%n"," * (A)tribuir / alterar rating por números","|");
-            System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
+            System.out.printf("|%-50s|%n", "");
+            System.out.printf("|%-50s|%n"," * Maracar como (V)isto por posição");
+            System.out.printf("|%-50s|%n"," * Marcar como visto por (t)ítulo");
+            System.out.printf("|%-50s|%n"," * (D)esmarcar último marcado como visto");
+            System.out.printf("|%-50s|%n"," * (A)tribuir / alterar rating por números");
+            System.out.printf("|%-50s|%n"," * (V)oltar");
+            System.out.printf("|%-50s|%n", "");
             System.out.println("+==================================================+");
             
-            System.out.print("\n\nEscolha sua opcao: ");
             opcao = lerChar();
+
+            switch (opcao) {
+            case ('v'):
+                menuPrincipal(tamMax, titulo, tipo, ano, visto, rating, nItens);
+                break;
+            
+        
+            default:
+                System.out.println("**** Opcao invalida ****\n\n");
+                break;
+            }
         
         }while(opcao!='v');
     }
 
     //------------------------------------------------Metodo: Menu Editar--------------------------------------------
-    static void menuEditar(){
-        //Este é o menu permite a adicionar ou eleminar conteudos.
+    static void menuEditar(int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
         char opcao = 'a';
         limparTela();
         do{
             System.out.println("+==================   MENU  =======================+");
-            System.out.printf("|%-25s %25s%n"," * (A)dicionar item no fim","|");
-            System.out.printf("|%-25s %25s%n"," * Adicionar (i)tem na posição n","|");
-            System.out.printf("|%-25s %25s%n"," * Apagar item na (p)osição n","|");
-            System.out.printf("|%-25s %25s%n"," * Apagar itens (v)istos","|");
-            System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
+            System.out.printf("|%-50s|%n", "");
+            System.out.printf("|%-50s|%n"," * (A)dicionar item no fim");
+            System.out.printf("|%-50s|%n"," * Adicionar (i)tem na posição n");
+            System.out.printf("|%-50s|%n"," * Apagar item na (p)osição n");
+            System.out.printf("|%-50s|%n"," * Apagar itens (v)istos");
+            System.out.printf("|%-50s|%n"," * (V)oltar");
+            System.out.printf("|%-50s|%n", "");
             System.out.println("+==================================================+");
             
-            System.out.print("\n\nEscolha sua opcao: ");
             opcao = lerChar();
+
+            switch (opcao) {
+            case ('v'):
+                menuPrincipal(tamMax, titulo, tipo, ano, visto, rating, nItens);
+                break;
+            
+        
+            default:
+                System.out.println("**** Opcao invalida ****\n\n");
+                break;
+            }
         
         }while(opcao!='v');
     }
 
     //------------------------------------------------Metodo: Menu Estatistica----------------------------------------
-    static void menuEstatisticas(){
-        //Este é o menu permite o utilizador escolher que estatisticas pretende ver.
+    static void menuEstatisticas(int tamMax, String titulo[], char tipo[], int ano[], boolean visto[], int rating[], int nItens){
         char opcao = 'a';
         limparTela();
         do{
             System.out.println("+==================   MENU  =======================+");
-            System.out.printf("|%-25s %25s%n"," * (P)ercentagem de vistos","|");
-            System.out.printf("|%-25s %25s%n"," * (M)édia de rating dos vistosn","|");
-            System.out.printf("|%-25s %25s%n"," * Distribuição por (d)écada","|");
-            System.out.printf("|%-25s %25s%n"," * (V)oltar","|");
+            System.out.printf("|%-50s|%n", "");
+            System.out.printf("|%-50s|%n"," * (P)ercentagem de vistos");
+            System.out.printf("|%-50s|%n"," * (M)édia de rating dos vistosn");
+            System.out.printf("|%-50s|%n"," * Distribuição por (d)écada");
+            System.out.printf("|%-50s|%n"," * (V)oltar");
+            System.out.printf("|%-50s|%n", "");
             System.out.println("+==================================================+");
             
-            System.out.print("\n\nEscolha sua opcao: ");
             opcao = lerChar();
+
+            switch (opcao) {
+            case ('v'):
+                menuPrincipal(tamMax, titulo, tipo, ano, visto, rating, nItens);
+                break;
+            
+        
+            default:
+                System.out.println("**** Opcao invalida ****\n\n");
+                break;
+            }
         
         }while(opcao!='v');
     }
 
 
-    //==============================     MAIN     ===============================================
+    //===============================================     MAIN     =================================================
 
     public static void main(String[] args) {
 
-    menuPrincipal();
+   
     int tamMax = 100;
     String[] titulo = new String[tamMax];
     char[] tipo = new char[tamMax];
     int[] ano = new int[tamMax];
     boolean[] visto = new boolean[tamMax];
     int[] rating = new int[tamMax];
-    int nItens=3;
+    int nItens=4;
     titulo[0] = "The Matrix"; tipo[0] = 'F'; ano[0] = 1999; visto[0] = true; rating[0] = 9;
     titulo[1] = "Breaking Bad"; tipo[1] = 'S'; ano[1] = 2008; visto[1] = true; rating[1] = 10;
     titulo[2] = "Oppenheimer"; tipo[2] = 'F'; ano[2] = 2023; visto[2] = false; rating[2] = 0;
     titulo[3] = "Dark"; tipo[3] = 'S'; ano[3] = 2017; visto[3] = false; rating[3] = 0;
-    fazedorTabela(tamMax, titulo, tipo, ano, visto, rating, nItens);
+
+    menuPrincipal(tamMax, titulo, tipo, ano, visto, rating, nItens);
+   
     }
 
         
